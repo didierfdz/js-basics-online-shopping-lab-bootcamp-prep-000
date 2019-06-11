99ec88847cb54d9d732d9b9cf7aeb83cf961bab9
Carlos Didier Fernandez <didierfdz@gmail.com>
2019-06-11 00:02:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ function removeFromCart(item) {
     var cartItem = eachItem.itemName;
     
     if (cartItem === item) {
-      ;
+      cart.splice(eachItem);
       return cart;
     } else {
       return 'That item is not in your cart.';
