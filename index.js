4cc8e1fe569cc008c1967b5abfa4f1b4e031d7b7
Carlos Didier Fernandez <didierfdz@gmail.com>
2019-06-11 00:24:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -86,4 +86,7 @@ function placeOrder(cardNumber) {
     
     return 'Your total cost is $' + sum + ', which will be charged to the card ' + card + '.'; 
   }
+  
+  var newCart = cart.slice();
+  return newCart;
 }
